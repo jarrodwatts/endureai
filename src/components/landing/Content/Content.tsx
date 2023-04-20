e08847053e1593c59d40856111a29e67c3f81377
Jarrod Watts <jarrodwatts16@gmail.com>
2023-04-20 13:18:44 +1000

    fuck with the image on homepage for responsiveness

--- src/components/landing/Content/Content.tsx
+++ src/components/landing/Content/Content.tsx
@@ -3,9 +3,17 @@ import styles from "./Content.module.css";
 import { useAuthContext } from "@/lib/context/AuthContext";
 import Image from "next/image";
 import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import theme from "@/lib/mui/theme";
 
 export default function Content() {
   const { user, loading } = useAuthContext();
+
+  const mobileScreen = useMediaQuery(theme.breakpoints.only("xs")); // xs only
+  const smScreen = useMediaQuery(theme.breakpoints.only("sm")); // sm only
+  const decentScreen = useMediaQuery(theme.breakpoints.up("md")); // md only
+
+  console.log({ mobileScreen, smScreen, decentScreen });
 
   return (
     <div className={styles.content}>
@@ -26,8 +34,11 @@ export default function Content() {
         <Image
           src={"/test2.png"}
           alt="Endure AI Hero Image"
-          width={820}
-          height={480}
+          // maintain 820x480 aspect ratio
+          width={mobileScreen ? 500 : smScreen ? 640 : decentScreen ? 820 : 820}
+          height={
+            mobileScreen ? 300 : smScreen ? 480 : decentScreen ? 480 : 480
+          }
           quality={100}
         />
       </div>
